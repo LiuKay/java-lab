4385efc794a4dbce94504164c84935617f337df3
LiuKay <passionno1@qq.com>
2020-04-25 13:15:05 +0800

    use CyclicBarrier to improve performance

--- src/main/java/com/kay/concurrency/aqs/CyclicBarrierDemo.java
+++ src/main/java/com/kay/concurrency/aqs/CyclicBarrierDemo.java
@@ -1,85 +1,117 @@
 package com.kay.concurrency.aqs;
 
-import java.util.Random;
-import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CyclicBarrier;
+import java.util.Vector;
+import java.util.concurrent.*;
 
-/**
- * Created by kay on 2017/9/12.
- */
+import static com.kay.concurrency.utils.TestUtils.log;
+import static com.kay.concurrency.utils.TestUtils.sleep;
+
 public class CyclicBarrierDemo {
-    public static class Soldier implements Runnable {
-
-        private String soldier;
-
-        private final CyclicBarrier cyclic;
-
-        Soldier(CyclicBarrier cyclic, String soldierName) {
-            this.cyclic=cyclic;
-            this.soldier=soldierName;
-        }
-
-        @Override
-        public void run() {
-            try {
-                cyclic.await();
-                doWork();
-                cyclic.await();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (BrokenBarrierException e) {
-                e.printStackTrace();
-            }
-        }
-
-        private void doWork() {
-            try {
-                Thread.sleep(Math.abs(new Random().nextInt() % 10000));
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println(soldier+":任务完成");
-        }
-    }
 
     /**
-     * 所有线程都到达屏障时，优先执行该 Runnable
+     *   业务场景：
+     *   对账：首先查询未对账订单，再查询未对账派送订单，拿到两者数据后进行对账
+     *
+     *   性能优化：
+     *   对账流程： T1执行订单查询，T2执行派单查询，T3执行对账，T1，T2并行，T1,T2执行完毕后执行T3做相应的对账，
+     *   不同对账流程之间可以并行
      */
-    public static class BarrierRun implements Runnable {
+    public static void main(String[] args) {
+        CheckTask.checkOrders(10);
+//        for (int i = 0; i < 10; i++) {
+//            CheckTask.checkOrdersTest(i);
+//        }
+    }
 
-        boolean flag;
-        int N;
+    static class CheckTask{
 
-        public BarrierRun(boolean flag, int N) {
-            this.flag=flag;
-            this.N=N;
+        // 思考：为什么要指定对账线程池线程数量为 1 ？
+        private static ExecutorService checkPool = Executors.newFixedThreadPool(1);
+
+        private static Vector<String> orderVector = new Vector<>();
+        private static Vector<String> deliverVector = new Vector<>();
+
+        //查询订单和派单完成后，回调对账
+        private static CyclicBarrier cyclicBarrier = new CyclicBarrier(2, CheckTask::check);
+
+        // 思考：为什么对账任务要单独起线程去做，如果直接在 CyclicBarrier的回调任务里面做有什么影响？ 回调任务是在哪个线程执行的？
+        private static void check(){
+            checkPool.execute(()->{
+                String order = orderVector.remove(0);
+                String deliver = deliverVector.remove(0);
+
+                log("执行对账,order=" + order + ",deliver=" + deliver);
+                sleep(1, TimeUnit.SECONDS);
+            });
         }
 
-        @Override
-        public void run() {
-            if (flag) {
-                System.out.println("士兵" + N + "个，任务完成！");
-            } else {
-                System.out.println("士兵" + N + "个，集合完成！");
-                flag = true;
+        private static String getOrders(int taskId) {
+            log("查询订单库,taskId=" + taskId);
+            sleep(1, TimeUnit.SECONDS);
+            return "order-" + taskId;
+        }
+
+        private static String getDelivers(int taskId) {
+            log("查询派送库,taskId=" + taskId);
+            sleep(2, TimeUnit.SECONDS);
+            return "deliver-" + taskId;
+        }
+
+        /**
+         *   TODO: for循环是为了测试，以下为正常流程
+         *    while (存在未对账订单){
+         *        String orders = getOrders(i);
+         *        orderVector.add(orders);
+         *        await();
+         *    }
+         */
+        public static void checkOrders(final int taskNum) {
+            //循环查询订单库
+            Thread t1 = new Thread(() -> {
+                for (int i = 0; i < taskNum; i++) {
+                    String orders = getOrders(i);
+                    orderVector.add(orders);
+                    await();
+                }
+            },"T-getOrder");
+            t1.start();
+
+            //循环查询派单库
+            Thread t2 = new Thread(() -> {
+                for (int i = 0; i < taskNum; i++) {
+                    String delivers = getDelivers(i);
+                    deliverVector.add(delivers);
+                    await();
+                }
+            }, "T-getDeliver");
+            t2.start();
+        }
+
+        //For another test
+        private static ExecutorService orderPool = Executors.newFixedThreadPool(1);
+        private static ExecutorService deliverPool = Executors.newFixedThreadPool(1);
+        public static void checkOrdersTest(final int taskNum) {
+            orderPool.execute(() -> {
+                String orders = getOrders(taskNum);
+                orderVector.add(orders);
+                await();
+            });
+
+            deliverPool.execute(() -> {
+                String delivers = getDelivers(taskNum);
+                deliverVector.add(delivers);
+                await();
+            });
+        }
+
+        private static void await() {
+            try {
+                cyclicBarrier.await();
+            } catch (InterruptedException | BrokenBarrierException e) {
+                e.printStackTrace();
             }
         }
     }
-
-    public static void main(String[] args) {
-        final int N=10;
-        Thread[] allSoldier = new Thread[N];
-        boolean flag = false;
-        CyclicBarrier cyclic = new CyclicBarrier(N, new BarrierRun(flag, N));
-
-        System.out.println("集合队伍");
-        for (int i=0;i<N;++i) {
-            System.out.println("士兵"+i+"报道");
-            allSoldier[i] = new Thread(new Soldier(cyclic, "士兵" + i));
-            allSoldier[i].start();
-        }
-    }
-
 
 
 }
--- src/main/java/com/kay/concurrency/utils/TestUtils.java
+++ src/main/java/com/kay/concurrency/utils/TestUtils.java
@@ -1,8 +1,12 @@
 package com.kay.concurrency.utils;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.TimeUnit;
 
 public final class TestUtils {
+
+    private static DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     public static void sleep(int time, TimeUnit timeUnit) {
         try {
@@ -10,6 +14,10 @@ public final class TestUtils {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void log(String msg) {
+        System.out.println(LocalDateTime.now().format(df) + " - [" + Thread.currentThread().getName() + "] - " + msg);
     }
 
 }
