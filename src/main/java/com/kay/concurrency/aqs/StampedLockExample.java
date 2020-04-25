863e216fd30fff3def892801f3424fef103e9ba7
LiuKay <passionno1@qq.com>
2020-04-25 09:34:09 +0800

    understand StampedLock

--- src/main/java/com/kay/concurrency/aqs/StampedLockExample.java
+++ src/main/java/com/kay/concurrency/aqs/StampedLockExample.java
@@ -3,6 +3,10 @@ package com.kay.concurrency.aqs;
 import java.util.concurrent.locks.StampedLock;
 
 /**
+ * StampedLock ：
+ * Write - exclusive lock
+ * Read - non-exclusive lock
+ * OptimisticRead - no lock
  * Created by kay on 2018/5/29.
  */
 public class StampedLockExample {
@@ -21,15 +25,15 @@ public class StampedLockExample {
             }
         }
 
-        //下面看看乐观读锁案例
+
         double distanceFromOrigin() { // A read-only method
-            long stamp = sl.tryOptimisticRead(); //获得一个乐观读锁
+            long stamp = sl.tryOptimisticRead(); // 乐观读，
             double currentX = x, currentY = y;  //将两个字段读入本地局部变量
             if (!sl.validate(stamp)) { //检查发出乐观读锁后同时是否有其他写锁发生？
-                stamp = sl.readLock();  //如果没有，我们再次获得一个读悲观锁
+                stamp = sl.readLock();  // 数据已经被修改，升级为读锁
                 try {
-                    currentX = x; // 将两个字段读入本地局部变量
-                    currentY = y; // 将两个字段读入本地局部变量
+                    currentX = x;
+                    currentY = y;
                 } finally {
                     sl.unlockRead(stamp);
                 }
@@ -37,20 +41,20 @@ public class StampedLockExample {
             return Math.sqrt(currentX * currentX + currentY * currentY);
         }
 
-        //下面是悲观读锁案例
+        //upgrade -锁的升级
         void moveIfAtOrigin(double newX, double newY) { // upgrade
             // Could instead start with optimistic, not read mode
             long stamp = sl.readLock();
             try {
                 while (x == 0.0 && y == 0.0) { //循环，检查当前状态是否符合
                     long ws = sl.tryConvertToWriteLock(stamp); //将读锁转为写锁
-                    if (ws != 0L) { //这是确认转为写锁是否成功
+                    if (ws != 0L) { //a valid write stamp, or zero on failure
                         stamp = ws; //如果成功 替换票据
-                        x = newX; //进行状态改变
-                        y = newY;  //进行状态改变
+                        x = newX;
+                        y = newY;
                         break;
                     } else { //如果不能成功转换为写锁
-                        sl.unlockRead(stamp);  //我们显式释放读锁
+                        sl.unlockRead(stamp);  //显式释放读锁
                         stamp = sl.writeLock();  //显式直接进行写锁 然后再通过循环再试
                     }
                 }
