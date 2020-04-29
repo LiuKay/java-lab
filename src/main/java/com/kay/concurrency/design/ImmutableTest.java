758f8d43bb45cf9442d31cc4071ae2b43e174b57
LiuKay <passionno1@qq.com>
2020-04-29 08:40:42 +0800

    Immutability 模式

--- src/main/java/com/kay/concurrency/design/ImmutableTest.java
+++ src/main/java/com/kay/concurrency/design/ImmutableTest.java
@@ -2,44 +2,95 @@ package com.kay.concurrency.design;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Maps;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
- * Created by kay on 2018/5/28.
+ * Immutability 模式需要注意的2个问题：
+ * 1. 不变的边界在哪里,是否要求属性也不可修改，对象的所有属性都是 final 的，并不能保证不可变性. 例如属性是一个集合类，集合的引用无法修改，但是其中的元素却是可以改变的。
+ * 2. 正确的发布对象
  */
 public class ImmutableTest {
 
-    private /*final*/ static Map<Integer, Integer> MAP = Maps.newHashMap();
-
-    public static final int i=1;
-
-    private static Map map = Collections.unmodifiableMap(MAP);
-
-    private static ImmutableList immutableList = ImmutableList.of("a","b","c");
-
-    private static ImmutableMap<String, String> immutableMap = ImmutableMap.<String, String>builder().put("k1", "v1").build();
-
-    static {
-        MAP.put(1, 12);
-    }
-
     public static void main(String[] args) {
-//       // i = 2
-//        MAP.put(1, 22);
-//        //提示不可变
-//        immutableList.add("d");
-//        //不提示
-//        map.put(3, 4);
+        //jdk provide
+        Map<String,String> jdkMap = Collections.unmodifiableMap(new HashMap<>());
 
-        List<String> initList = Arrays.asList("A", "B", "C");
+         //guava
+        ImmutableList<String> immutableList = ImmutableList.of("a","b","c");
+        ImmutableMap<String, String> immutableMap = ImmutableMap.<String, String>builder().put("k1", "v1").build();
+//        immutableList.add("d"); //Deprecated
+//        jdkMap.put("a", "b"); //不提示
+
+        //自己实现的
         MyImmutableArrayList<String> list = MyImmutableArrayList.of("A", "B", "C");
-        list.add("D"); // UnsupportedOperationException
+//        list.add("D"); // UnsupportedOperationException
         Iterator<String> iterator = list.iterator();
         while (iterator.hasNext()) {
             System.out.println(iterator.next());
 //            iterator.remove(); // UnsupportedOperationException
         }
     }
+
+    //不可变对象
+    private static final class Foo{
+        private final int age;
+        public Foo(int age) {
+            this.age = age;
+        }
+        public final int getAge(){
+            return this.age;
+        }
+        //修改时返回新的不可变对象
+        public Foo setAge(int age) {
+            return new Foo(age);
+        }
+
+        // 利用享元模式来缓存经常使用的对象，减少对象创建开销，类似于 Integer,Long 里面的缓存
+        public static Foo valueOf(int age){
+            if(age<0){
+                throw new IllegalArgumentException();
+            }
+            if (age < 100) {
+                return FooCache.cache[age];
+            }
+            return new Foo(age);
+        }
+
+        private static class FooCache{
+            private FooCache(){}
+            static final Foo[] cache = new Foo[100];
+            static {
+                for (int i = 0; i < cache.length; i++) {
+                    cache[i] = new Foo(i);
+                }
+            }
+        }
+    }
+
+    //线程不安全对象
+    private class Bar {
+        private Foo foo;
+        //虽然 Foo类本身是 final的，但是在多线程并发下，此处无法保证对 foo修改的原子性和可见性
+        void setFoo(Foo f){
+            this.foo = f;
+        }
+    }
+
+    private class SafeBar{
+        private final AtomicReference<Foo> atomicReference = new AtomicReference<>(new Foo(0));
+        void setFoo(Foo foo){
+            while (true) {
+                Foo foo1 = atomicReference.get();
+                if (atomicReference.compareAndSet(foo1, foo)) {
+                    return;
+                }
+            }
+        }
+    }
+
 }
