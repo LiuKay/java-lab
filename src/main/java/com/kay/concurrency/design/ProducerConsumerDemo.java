5407392d1e6cb47c29b4a1baad4da4588579039f
LiuKay <passionno1@qq.com>
2020-05-06 20:48:31 +0800

    Producer - Consumer: case 2-> 分段提交案例

--- src/main/java/com/kay/concurrency/design/ProducerConsumerDemo.java
+++ src/main/java/com/kay/concurrency/design/ProducerConsumerDemo.java
@@ -1,6 +1,10 @@
 package com.kay.concurrency.design;
 
 import com.kay.concurrency.utils.NamedThreadFactory;
+import com.kay.concurrency.utils.TestUtils;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
@@ -12,6 +16,7 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.extern.log4j.Log4j2;
 
 /**
@@ -23,22 +28,26 @@ import lombok.extern.log4j.Log4j2;
 public class ProducerConsumerDemo {
 
     public static void main(String[] args) {
-        BatchService batchService=new BatchService();
-        batchService.testBatchExecute();
+//        BatchService batchService=new BatchService();
+//        batchService.testBatchExecute();
+
+        SectionCommit sectionCommit = new SectionCommit();
+        sectionCommit.testLogService();
     }
 
     /**
      * Case 1 : batch execute tasks
      */
-    static class BatchService{
-        private static BlockingQueue<Task> queue = new LinkedBlockingQueue(2000);
+    static class BatchService {
+
+        private static BlockingQueue<Task> queue = new LinkedBlockingQueue<>(1000);
 
         public void testBatchExecute() {
             ExecutorService service = Executors
                 .newFixedThreadPool(5, NamedThreadFactory.namedThreadFactory("BatchExecutor"));
 
             for (int i = 0; i < 5; i++) {
-                service.execute(()->{
+                service.execute(() -> {
                     try {
                         while (true) {
                             List<Task> tasks = pollTasks();
@@ -77,10 +86,121 @@ public class ProducerConsumerDemo {
 
         @Data
         @AllArgsConstructor
-        static class Task{
+        static class Task {
+
             private UUID id;
             private String content;
         }
     }
 
+    /**
+     * Case 2: 分段提交 案例：日志刷盘 规则： 1.ERROR 立即刷盘 2. 累积 500条刷盘 3. 5秒内有未刷盘日志立即刷盘
+     */
+    static class SectionCommit {
+
+        private final LogMsg POISON_MSG = new LogMsg(LEVEL.ERROR, "POISON-LogMsg");
+
+        BlockingQueue<LogMsg> queue = new LinkedBlockingQueue<>(2000);
+        ExecutorService es = Executors
+            .newFixedThreadPool(1, NamedThreadFactory.namedThreadFactory("LOG-flush"));
+
+        void testLogService() {
+            try {
+                flush();
+            } catch (IOException e) {
+                log.error(e);
+            }
+
+            ScheduledExecutorService errorService = Executors
+                .newSingleThreadScheduledExecutor(NamedThreadFactory.namedThreadFactory("Log-e"));
+
+            errorService.scheduleWithFixedDelay(() -> error("e"), 10, 10, TimeUnit.SECONDS);
+
+            ScheduledExecutorService infoService = Executors
+                .newSingleThreadScheduledExecutor(NamedThreadFactory.namedThreadFactory("Log-i"));
+
+            infoService.scheduleWithFixedDelay(() -> info("info msg"), 1, 1, TimeUnit.SECONDS);
+
+            TestUtils.sleep(15, TimeUnit.SECONDS);
+            infoService.shutdown();
+            errorService.shutdown();
+            stop();
+        }
+
+        void flush() throws IOException {
+            File file = File.createTempFile("test", ".log");
+            FileWriter writer = new FileWriter(file);
+            es.execute(() -> {
+                try {
+                    int sumMsg = 0;
+                    long start = System.currentTimeMillis();
+                    while (true) {
+                        LogMsg msg = queue.poll(5, TimeUnit.SECONDS);
+
+                        if (msg != null) {
+                            if (POISON_MSG.equals(msg)) {
+                                break;
+                            }
+                            writer.write(msg.toString());
+                            sumMsg++;
+                        }
+
+                        long current = System.currentTimeMillis();
+                        // flush condition
+                        if (msg != null && (msg.level == LEVEL.ERROR || sumMsg >= 500
+                            || (current - start) >= 5000)) {
+                            log.info("flush logs.");
+                            writer.flush();
+                            sumMsg = 0;
+                            start = System.currentTimeMillis();
+                        }
+
+                    }
+                } catch (Exception e) {
+                    log.error(e);
+                } finally {
+                    try {
+                        writer.flush();
+                        writer.close();
+                        es.shutdown(); //TODO: Is it reasonable?
+                    } catch (IOException e) {
+                        log.error(e);
+                    }
+                }
+            });
+        }
+
+        enum LEVEL {
+            INFO, ERROR
+        }
+
+        @Data
+        @AllArgsConstructor
+        @EqualsAndHashCode
+        class LogMsg {
+
+            private LEVEL level;
+            private String msg;
+
+            @Override
+            public String toString() {
+                return "LogMsg{level=" + level + ", msg=" + msg + "}\n";
+            }
+        }
+
+        void info(String msg) {
+            log.info("add info msg");
+            queue.add(new LogMsg(LEVEL.INFO, msg));
+        }
+
+        void error(String msg) {
+            log.info("add error msg");
+            queue.add(new LogMsg(LEVEL.ERROR, msg));
+        }
+
+        // poison is used to stop the log thread
+        private void stop() {
+            queue.add(POISON_MSG);
+        }
+    }
 }
