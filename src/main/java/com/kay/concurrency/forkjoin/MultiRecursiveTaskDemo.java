6c6057572a895114a0ece21e66ccdefb5032d28e
LiuKay <passionno1@qq.com>
2020-05-07 09:31:42 +0800

    ForkJoin demo for invokeAll()

---
+++ src/main/java/com/kay/concurrency/forkjoin/MultiRecursiveTaskDemo.java
@@ -0,0 +1,68 @@
+package com.kay.concurrency.forkjoin;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveTask;
+
+/**
+ * Created on 5/7/2020 Demo for invokeAll()
+ *
+ * @author: LiuKay
+ */
+public class MultiRecursiveTaskDemo {
+
+    public static void main(String[] args) {
+        int[] arr = new int[50];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+        }
+        ForkJoinPool pool = new ForkJoinPool(4);
+        CustomRecursiveTask task = new CustomRecursiveTask(arr);
+        Integer integer = pool.invoke(task);
+        System.out.println(integer);
+    }
+
+    static class CustomRecursiveTask extends RecursiveTask<Integer> {
+
+        private int[] arr;
+
+        private static final int THRESHOLD = 20;
+
+        public CustomRecursiveTask(int[] arr) {
+            this.arr = arr;
+        }
+
+        @Override
+        protected Integer compute() {
+            if (arr.length > THRESHOLD) {
+                return ForkJoinTask.invokeAll(createSubTasks())
+                    .stream()
+                    .mapToInt(ForkJoinTask::join)
+                    .sum();
+            } else {
+                return processing(arr);
+            }
+        }
+
+        private Collection<CustomRecursiveTask> createSubTasks() {
+            List<CustomRecursiveTask> dividedTasks = new ArrayList<>();
+            dividedTasks.add(new CustomRecursiveTask(
+                Arrays.copyOfRange(arr, 0, arr.length / 2)));
+            dividedTasks.add(new CustomRecursiveTask(
+                Arrays.copyOfRange(arr, arr.length / 2, arr.length)));
+            return dividedTasks;
+        }
+
+        private Integer processing(int[] arr) {
+            return Arrays.stream(arr)
+                .filter(a -> a > 10 && a < 27)
+                .map(a -> a * 10)
+                .sum();
+        }
+    }
+
+}
