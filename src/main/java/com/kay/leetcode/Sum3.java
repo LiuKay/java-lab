d9d182a91a219d66fc476bb93708081736aced71
LiuKay <passionno1@qq.com>
2020-05-20 21:44:27 +0800

    add 3Sum

---
+++ src/main/java/com/kay/leetcode/Sum3.java
@@ -0,0 +1,66 @@
+package com.kay.leetcode;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created on 2020/5/20
+ *
+ * @author: LiuKay
+ */
+public class Sum3 {
+
+    //Given an array nums of n integers, are there elements a, b, c in nums such tha
+//t a + b + c = 0? Find all unique triplets in the array which gives the sum of ze
+//ro.
+//
+// Note:
+//
+// The solution set must not contain duplicate triplets.
+//
+// Example:
+//
+//
+//Given array nums = [-1, 0, 1, 2, -1, -4],
+//
+//A solution set is:
+//[
+//  [-1, 0, 1],
+//  [-1, -1, 2]
+//]
+//
+// Related Topics Array Two Pointers
+    class Solution {
+        public List<List<Integer>> threeSum(int[] nums) {
+            Arrays.sort(nums);
+            List<List<Integer>> result = new LinkedList<>();
+            for (int i = 0; i < nums.length - 2; i++) {
+                if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+                    int lo = i + 1;
+                    int hi=nums.length -1;
+                    int sum = -nums[i];
+
+                    while (lo < hi){
+                        if (nums[lo] + nums[hi] == sum) {
+                            result.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+                            while (lo < hi && nums[lo] == nums[lo + 1]) {
+                                lo++;
+                            }
+                            while (lo < hi && nums[hi] == nums[hi - 1]) {
+                                hi--;
+                            }
+                            lo++;
+                            hi--;
+                        } else if (nums[lo] + nums[hi] < sum) {
+                            lo++;
+                        }else {
+                            hi--;
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
