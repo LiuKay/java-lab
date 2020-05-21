4f7ee1433d65511a04d6e069f33cf261d3187c84
LiuKay <passionno1@qq.com>
2020-05-21 09:55:32 +0800

    1.two pointers,2.hash set: more about 3Sum

--- src/main/java/com/kay/leetcode/Sum3.java
+++ src/main/java/com/kay/leetcode/Sum3.java
@@ -32,35 +32,80 @@ public class Sum3 {
 //
 // Related Topics Array Two Pointers
     class Solution {
+
         public List<List<Integer>> threeSum(int[] nums) {
             Arrays.sort(nums);
             List<List<Integer>> result = new LinkedList<>();
-            for (int i = 0; i < nums.length - 2; i++) {
-                if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
-                    int lo = i + 1;
-                    int hi=nums.length -1;
-                    int sum = -nums[i];
-
-                    while (lo < hi){
-                        if (nums[lo] + nums[hi] == sum) {
-                            result.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
-                            while (lo < hi && nums[lo] == nums[lo + 1]) {
-                                lo++;
-                            }
-                            while (lo < hi && nums[hi] == nums[hi - 1]) {
-                                hi--;
-                            }
-                            lo++;
-                            hi--;
-                        } else if (nums[lo] + nums[hi] < sum) {
-                            lo++;
-                        }else {
-                            hi--;
-                        }
-                    }
+            for (int i = 0; i < nums.length && nums[i] <= 0; i++) {
+                if (i == 0 || nums[i] != nums[i - 1]) {
+                    twoSum(nums, result, i);
                 }
             }
             return result;
         }
+
+        private void twoSum(int[] nums, List<List<Integer>> result, int i) {
+            int lo = i + 1;
+            int hi = nums.length - 1;
+            int sum = nums[lo] + nums[i] + nums[hi];
+            while (lo < hi) {
+                if (sum < 0 || nums[lo] == nums[lo - 1]) {
+                    lo++;
+                } else if (sum > 0 || nums[hi] == nums[hi + 1]) {
+                    hi--;
+                } else {
+                    result.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+                }
+            }
+        }
     }
+
+    /**
+     * Solution:
+     * 1. Two pointers
+     * Algorithm
+     *
+     * The implementation is straightforward - we just need to modify twoSumII to produce triplets and skip repeating values.
+     *
+     * For the main function:
+     *
+     * Sort the input array nums.
+     * Iterate through the array:
+     * If the current value is greater than zero, break from the loop. Remaining values cannot sum to zero.
+     * If the current value is the same as the one before, skip it.
+     * Otherwise, call twoSumII for the current position i.
+     * For twoSumII function:
+     *
+     * Set the low pointer lo to i + 1, and high pointer hi to the last index.
+     * While low pointer is smaller than high:
+     * If the sum of nums[i], nums[lo] and nums[hi] is less than zero, increment lo.
+     * Also increment lo if the value is the same as for lo - 1.
+     * If the sum is greater than zero, decrement hi.
+     * Also decrement hi if the value is the same as for hi + 1.
+     * Otherwise, we found a triplet:
+     * Add it to the result res.
+     * Decrement hi and increment lo.
+     * Return the result res.
+     *
+     *
+     * 2.Hash Set
+     * Since triplets must sum up to the target value, we can try the hash table approach from the Two Sum solution. This approach won't work, however, if the sum needs to be smaller than a target, like in 3Sum Smaller.
+     *
+     * Handling duplicates here is trickier compared to the two pointers approach. We can put a combination of three values into a hash set to efficiently check whether we've found that triplet already. Values in a combination should be ordered (e.g. ascending). Otherwise, we can have results with the same values in the different positions.
+     *
+     * Fortunately, we do not need to store all three values - storing the smallest and the largest ones is sufficient to identify any triplet. Because three values sum to the target, the third value will always be the same.
+     *
+     * Algorithm
+     *
+     * We process each value from left to right. For value v, we need to find all pairs whose sum is equal -v. To find such pairs, we apply the Two Sum: One-pass Hash Table approach to the rest of the array. To ensure unique triplets, we use a hash set found as described above.
+     *
+     * Because hashmap operations could be expensive, the solution below may be too slow.
+     *
+     * =>Optimized Algorithm
+     *
+     * These optimizations don't change the big-O complexity, but help speed things up:
+     *
+     * Use another hash set dups to skip duplicates in the outer loop.
+     * Instead of re-populating a hash set every time in the inner loop, we can populate a hashmap once and then only modify values. After we process nums[j] in the inner loop, we set the hashmap value to i. This indicates that we can now use nums[j] to find pairs for nums[i].
+     */
 }
