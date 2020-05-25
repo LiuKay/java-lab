248cbca01b94cb915641c1544b667a97f1504ca5
LiuKay <passionno1@qq.com>
2020-05-25 08:04:17 +0800

    add join() demo

---
+++ src/main/java/com/kay/concurrency/thread/JoinDemo.java
@@ -0,0 +1,41 @@
+package com.kay.concurrency.thread;
+
+import com.kay.concurrency.utils.TestUtils;
+import java.util.concurrent.TimeUnit;
+import lombok.extern.log4j.Log4j2;
+
+/**
+ * Created by LiuKay on 2020/5/24.
+ */
+@Log4j2
+public class JoinDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        log.info(Thread.currentThread().getName() + " 正在执行");
+        Thread t1=new Thread(()->{
+            log.info("aaa");
+            TestUtils.sleep(2, TimeUnit.SECONDS);
+            log.info(Thread.currentThread().getName() + " 正在执行");
+        },"t1");
+
+        Thread t2=new Thread(()->{
+            try {
+                t1.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            log.info("bbb");
+            TestUtils.sleep(2, TimeUnit.SECONDS);
+            log.info(Thread.currentThread().getName() + " 正在执行");
+        },"t2");
+
+        t1.start();
+        t2.start();
+        t2.join();
+
+        log.info(Thread.currentThread().getName() + " 正在执行");
+    }
+
+}
