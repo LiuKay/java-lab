c85ce2f5143a9335674af2db9f2ccec755052b41
LiuKay <passionno1@qq.com>
2020-06-05 22:20:48 +0800

    leetcode: linked list has a cycle II

--- src/main/java/com/kay/leetcode/linkedlist/LinkedListCycleII.java
+++ src/main/java/com/kay/leetcode/linkedlist/LinkedListCycleII.java
@@ -1,5 +1,7 @@
 package com.kay.leetcode.linkedlist;
 
+import com.kay.leetcode.ListNode;
+
 /**
  * Created on 2020/5/23
  *
@@ -7,4 +9,21 @@ package com.kay.leetcode.linkedlist;
  */
 public class LinkedListCycleII {
 
+    public class Solution {
+        public ListNode detectCycle(ListNode head) {
+            ListNode fast = head, slow = head;
+            while (true) {
+                if (fast == null || fast.next == null) return null;
+                fast = fast.next.next;
+                slow = slow.next;
+                if (fast == slow) break;
+            }
+            fast = head;
+            while (slow != fast) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+            return fast;
+        }
+    }
 }
