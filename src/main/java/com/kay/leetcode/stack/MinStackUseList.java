213ded5a49db8e457aa0e8310d036c7eff6db232
LiuKay <passionno1@qq.com>
2020-06-18 21:11:20 +0800

    leetcode: min stack

---
+++ src/main/java/com/kay/leetcode/stack/MinStackUseList.java
@@ -0,0 +1,45 @@
+package com.kay.leetcode.stack;
+
+class MinStackUseList {
+
+    private static class Node{
+        int val;
+        int min;
+        Node next;
+
+        public Node(int val, int min) {
+            this(val, min, null);
+        }
+
+        public Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+
+    private Node head;
+
+    public MinStackUseList() {
+    }
+
+    void push(int x){
+        if (head == null) {
+            head = new Node(x, x);
+        }else {
+            head = new Node(x, Math.min(x, head.min), head);
+        }
+    }
+
+    void pop(){
+        head = head.next;
+    }
+
+    int top(){
+       return head.val;
+    }
+
+    int getMin(){
+        return head.min;
+    }
+}
