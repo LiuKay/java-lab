3a7065eda3524f27a547dc7ccbcc8d7d8d164033
LiuKay <passionno1@qq.com>
2020-06-19 21:52:50 +0800

    how to terminate thread properly

--- src/main/java/com/kay/concurrency/thread/TestInterrupt.java
+++ src/main/java/com/kay/concurrency/thread/TestInterrupt.java
@@ -1,13 +1,24 @@
 package com.kay.concurrency.thread;
 
+import com.kay.concurrency.design.TerminateThreadDemo;
+import lombok.extern.log4j.Log4j2;
+
 /**
  *  Catch an InterruptedException will make the interrupted status of the thread be cleared.
+ *  The task A will keep running.
+ *
+ *  How to terminate thread properly see {@link TerminateThreadDemo )
  */
+@Log4j2
 public class TestInterrupt {
 
     public static void main(String[] args) {
         Thread threadA = new Thread(new TaskA(), "thread A");
         threadA.start();
+
+        TaskB taskB = new TaskB();
+        Thread threadB = new Thread(taskB, "thread B");
+        threadB.start();
 
         try {
             Thread.sleep(300);
@@ -15,6 +26,7 @@ public class TestInterrupt {
             e.printStackTrace();
         }
         threadA.interrupt();
+        taskB.stop();
     }
 
     static class TaskA implements Runnable{
@@ -33,8 +45,33 @@ public class TestInterrupt {
                 } catch (InterruptedException e) {
                     System.out.println(Thread.currentThread().getName()+" has a InterruptedException:");
                     e.printStackTrace();
+//                    Thread.currentThread().interrupt(); //if this line is not invoke properly,the thread will keep running
                 }
             }
+        }
+    }
+
+    static class TaskB implements Runnable{
+
+        private volatile boolean stop;
+
+        void stop(){
+            this.stop = true;
+        }
+
+        @Override
+        public void run() {
+            while (!stop) {
+                System.out.println(Thread.currentThread().getName() + " is running.");
+
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    System.out.println(Thread.currentThread().getName()+" has a InterruptedException:");
+                    e.printStackTrace();
+                }
+            }
+            log.info("Task B is stopped.");
         }
     }
 }
