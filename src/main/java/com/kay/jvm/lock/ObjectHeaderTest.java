6d45fe85ea2d777e52de11ed619135f892f6c4be
LiuKay <passionno1@qq.com>
2020-06-21 10:37:31 +0800

    jvm lock test

---
+++ src/main/java/com/kay/jvm/lock/ObjectHeaderTest.java
@@ -0,0 +1,18 @@
+package com.kay.jvm.lock;
+
+import org.openjdk.jol.info.ClassLayout;
+
+/**
+ * -XX:+UseBiasedLocking -XX:BiasedLockingStartupDelay=0
+ */
+public class ObjectHeaderTest {
+
+    public static void main(String[] args) {
+        TestObject testObject = new TestObject();
+        System.out.println(ClassLayout.parseInstance(testObject).toPrintable());
+    }
+
+    static class TestObject{
+        private int flag;
+    }
+}
