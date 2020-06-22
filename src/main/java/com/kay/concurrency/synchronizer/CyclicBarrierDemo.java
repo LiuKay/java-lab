04994c97988e6271a299f51ac8b0dfa4c023f1ac
LiuKay <passionno1@qq.com>
2020-06-22 10:08:01 +0800

    clean code: 利用 CyclicBarrier 实现一个对账功能

--- src/main/java/com/kay/concurrency/synchronizer/CyclicBarrierDemo.java
+++ src/main/java/com/kay/concurrency/synchronizer/CyclicBarrierDemo.java
@@ -3,6 +3,8 @@ package com.kay.concurrency.synchronizer;
 import static com.kay.concurrency.utils.Utils.sleep;
 
 import java.util.Vector;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.ExecutorService;
@@ -10,22 +12,32 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import lombok.extern.log4j.Log4j2;
 
+/**
+ * 业务场景：
+ * 对未对账的订单进行对账，未对账订单是一个增量的任务，可以理解为在一个 MQ里不断接收新的订单
+ * 对账条件：
+ * 1.未对账订单，
+ * 2.未对账派送单，
+ * 3.拿到两者数据后进行对账
+ * <p>
+ * 性能优化：
+ * 对账流程： T1执行订单查询，T2执行派单查询，T3执行对账，T1，T2并行，T1,T2执行完毕后执行T3做相应的对账，
+ * 不同对账流程之间可以并行
+ */
 @Log4j2
 public class CyclicBarrierDemo {
 
-    /**
-     *   业务场景：
-     *   对账：首先查询未对账订单，再查询未对账派送订单，拿到两者数据后进行对账
-     *
-     *   性能优化：
-     *   对账流程： T1执行订单查询，T2执行派单查询，T3执行对账，T1，T2并行，T1,T2执行完毕后执行T3做相应的对账，
-     *   不同对账流程之间可以并行
-     */
+    //用来模拟增量的为对账订单,此处应该是一个 MQ, 有2个订阅，为了简单所以用2个相同数据的queue模拟一下
+    private static BlockingQueue<Integer> test1Data = new ArrayBlockingQueue<>(10);
+    private static BlockingQueue<Integer> test2Data = new ArrayBlockingQueue<>(10);
+
     public static void main(String[] args) {
-        CheckTask.checkOrders(10);
-//        for (int i = 0; i < 10; i++) {
-//            CheckTask.checkOrdersTest(i);
-//        }
+        for (int i = 0; i < 10; i++) {
+            test1Data.add(i);
+            test2Data.add(i);
+        }
+
+        CheckTask.checkOrders();
     }
 
     static class CheckTask{
@@ -40,8 +52,8 @@ public class CyclicBarrierDemo {
         private static CyclicBarrier cyclicBarrier = new CyclicBarrier(2, CheckTask::check);
 
         // 思考：为什么对账任务要单独起线程去做，如果直接在 CyclicBarrier的回调任务里面做有什么影响？ 回调任务是在哪个线程执行的？
-        private static void check(){
-            checkPool.execute(()->{
+        private static void check() {
+            checkPool.execute(() -> {
                 String order = orderVector.remove(0);
                 String deliver = deliverVector.remove(0);
 
@@ -50,66 +62,55 @@ public class CyclicBarrierDemo {
             });
         }
 
-        private static String getOrders(int taskId) {
-            log.info("查询订单库,taskId={}", taskId);
-            sleep(1, TimeUnit.SECONDS);
-            return "order-" + taskId;
-        }
-
-        private static String getDelivers(int taskId) {
-            log.info("查询派送库,taskId={}", taskId);
-            sleep(2, TimeUnit.SECONDS);
-            return "deliver-" + taskId;
-        }
-
-        /**
-         *   TODO: for循环是为了测试，以下为正常流程
-         *    while (存在未对账订单){
-         *        String orders = getOrders(i);
-         *        orderVector.add(orders);
-         *        await();
-         *    }
-         */
-        public static void checkOrders(final int taskNum) {
-            //循环查询订单库
-            Thread t1 = new Thread(() -> {
-                for (int i = 0; i < taskNum; i++) {
-                    String orders = getOrders(i);
-                    orderVector.add(orders);
-                    await();
+        private static Runnable getUncheckedOrder() {
+            return () -> {
+                while (true) {
+                    Integer taskId = null;
+                    try {
+                        taskId = test1Data.take();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    log.info("查询订单库,taskId={}", taskId);
+                    sleep(1, TimeUnit.SECONDS);
+                    String order = "order-" + taskId;
+                    orderVector.add(order);
+                    awaitForCheck();
                 }
-            },"T-getOrder");
+            };
+        }
+
+        private static Runnable getUncheckedDeliver() {
+            return () -> {
+                while (true) {
+                    Integer taskId = null;
+                    try {
+                        taskId = test2Data.take();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    sleep(2, TimeUnit.SECONDS);
+                    log.info("查询派送库,taskId={}", taskId);
+                    String deliver = "deliver-" + taskId;
+                    deliverVector.add(deliver);
+                    awaitForCheck();
+                }
+            };
+        }
+
+        public static void checkOrders() {
+            //一个线程循环查询订单库
+            Thread t1 = new Thread(getUncheckedOrder(), "T-getOrder");
             t1.start();
 
-            //循环查询派单库
-            Thread t2 = new Thread(() -> {
-                for (int i = 0; i < taskNum; i++) {
-                    String delivers = getDelivers(i);
-                    deliverVector.add(delivers);
-                    await();
-                }
-            }, "T-getDeliver");
+            //另一个线程循环查询派单库
+            Thread t2 = new Thread(getUncheckedDeliver(), "T-getDeliver");
             t2.start();
+
+            //当 cyclicBarrier 被触发后会回调 barrierAction(即check()方法进行对账)
         }
 
-        //For another test
-        private static ExecutorService orderPool = Executors.newFixedThreadPool(1);
-        private static ExecutorService deliverPool = Executors.newFixedThreadPool(1);
-        public static void checkOrdersTest(final int taskNum) {
-            orderPool.execute(() -> {
-                String orders = getOrders(taskNum);
-                orderVector.add(orders);
-                await();
-            });
-
-            deliverPool.execute(() -> {
-                String delivers = getDelivers(taskNum);
-                deliverVector.add(delivers);
-                await();
-            });
-        }
-
-        private static void await() {
+        private static void awaitForCheck() {
             try {
                 cyclicBarrier.await();
             } catch (InterruptedException | BrokenBarrierException e) {
